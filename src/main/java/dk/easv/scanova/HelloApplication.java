f2a8f15eadb64885008fc2e61a8376805217b75c
AhmedCali <ahmfar01@easv365.dk>
2026-04-22 11:19:14 +0200

    Test

--- src/main/java/dk/easv/scanova/HelloApplication.java
+++ src/main/java/dk/easv/scanova/HelloApplication.java
@@ -1,4 +1,4 @@
-package dk.easv.scanova.GUI;
+package dk.easv.scanova;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/dk/easv/scanova/HelloController.java
+++ src/main/java/dk/easv/scanova/HelloController.java
@@ -1,4 +1,4 @@
-package dk.easv.scanova.GUI;
+package dk.easv.scanova;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
