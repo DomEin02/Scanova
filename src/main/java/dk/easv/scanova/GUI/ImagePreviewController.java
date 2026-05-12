f13da301dd48409e026d36a55b48fe087f5ff1e8
Elena Borgwardt Vahlun <Elena.Borgwardt@hotmail.com>
2026-05-12 19:42:17 +0200

    rotate scrollbar

--- src/main/java/dk/easv/scanova/GUI/ImagePreviewController.java
+++ src/main/java/dk/easv/scanova/GUI/ImagePreviewController.java
@@ -5,15 +5,44 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import dk.easv.scanova.DAL.FileDAO;
 import dk.easv.scanova.Model.ScannedFile;
+import javafx.scene.control.Slider;
 
 public class ImagePreviewController {
 
     @FXML
     private ImageView imageView;
 
+    @FXML
+    private Slider rotationSlider;
+
     private int currentRotation = 0;
+
     private ScannedFile currentFile;
+
     private final FileDAO fileDAO = new FileDAO();
+
+    @FXML
+    private void initialize() {
+
+        rotationSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+
+            currentRotation = ((int) Math.round(newVal.doubleValue() / 5)) * 5;
+
+            imageView.setRotate(currentRotation);
+        });
+
+        rotationSlider.setOnMouseReleased(event -> {
+
+            if (currentFile != null &&
+
+                    currentFile.getRotation() != currentRotation) {
+
+                currentFile.setRotation(currentRotation);
+
+                fileDAO.updateRotation(currentFile.getFileId(), currentRotation);
+            }
+        });
+    }
 
     public void setImage(Image image, ScannedFile file) {
 
@@ -24,29 +53,7 @@ public class ImagePreviewController {
         currentRotation = file.getRotation();
 
         imageView.setRotate(currentRotation);
-    }
 
-    @FXML
-    private void rotateRight() {
-
-        currentRotation = (currentRotation + 5) % 360;
-
-        imageView.setRotate(currentRotation);
-
-        currentFile.setRotation(currentRotation);
-
-        fileDAO.updateRotation(currentFile.getFileId(), currentRotation);
-    }
-
-    @FXML
-    private void rotateLeft() {
-
-        currentRotation = (currentRotation + 355) % 360;
-
-        imageView.setRotate(currentRotation);
-
-        currentFile.setRotation(currentRotation);
-
-        fileDAO.updateRotation(currentFile.getFileId(), currentRotation);
+        rotationSlider.setValue(currentRotation);
     }
 }
